0eb74b0fa42162f1d8722656edfb8ce020e6f842
Shrey <shreyshukla1010@gmail.com>
2024-06-23 12:54:36 +0530

    setup graphene in django

--- graphql_social_media_api/graphql_social_media_api/settings.py
+++ graphql_social_media_api/graphql_social_media_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "graphql_api",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GRAPHENE = {
+    "SCHEMA": "graphql_social_media_api.schema.schema"
+}
